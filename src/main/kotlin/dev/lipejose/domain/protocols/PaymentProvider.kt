feb4a59937541f9f8bdedbd402ad468731fa4b30
Felipe José <lipejose@icloud.com>
2022-03-11 12:13:32 -0300

    refactor: payment protocol and service

--- src/main/kotlin/dev/lipejose/domain/protocols/PaymentProvider.kt
+++ src/main/kotlin/dev/lipejose/domain/protocols/PaymentProvider.kt
@@ -2,7 +2,8 @@ package dev.lipejose.domain.services.protocols
 
 import dev.lipejose.PaymentResponse
 import dev.lipejose.domain.entities.Card
+import dev.lipejose.domain.entities.Order
 
 interface PaymentProvider {
-    fun execute(card: Card, amount: String): PaymentResponse
+    fun execute(card: Card, order: Order): PaymentResponse
 }
--- src/main/kotlin/dev/lipejose/domain/services/PaymentService.kt
+++ src/main/kotlin/dev/lipejose/domain/services/PaymentService.kt
@@ -3,11 +3,19 @@ package dev.lipejose.domain.services
 import dev.lipejose.PaymentRequest
 import dev.lipejose.PaymentResponse
 import dev.lipejose.domain.entities.Card
+import dev.lipejose.domain.entities.Customer
+import dev.lipejose.domain.entities.Order
 import dev.lipejose.domain.services.protocols.PaymentProvider
 
 class PaymentService(private val provider: PaymentProvider) {
-    fun process(payment: PaymentRequest): PaymentResponse {
-        val card = Card(payment.number, payment.ccv, payment.exp, payment.name)
-        return provider.execute(card, payment.amount)
+    fun process(request: PaymentRequest): PaymentResponse {
+        val card = Card(request.card.number, request.card.ccv, request.card.exp, request.card.name, request.card.brand)
+        val customer =  Customer(request.order.customer.firstName, request.order.customer.lastName)
+        val order = Order(request.order.orderId, request.order.amount, customer)
+        println(order)
+        println(card)
+        println(customer)
+
+        return provider.execute(card, order)
     }
 }
